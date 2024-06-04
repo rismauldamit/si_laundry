40719a23b954de9ca422ed26d0ef298e910b41a4
rismauldamit <rismaulmagfiroh.plh123@gmail.com>
2024-06-04 14:05:42 +0800

    14.05

--- app/Views/barang_laundry.php
+++ app/Views/barang_laundry.php
@@ -7,7 +7,67 @@
   </nav>
 </div>
 
-<div class="section-body"> 
+<main id="main" class="main">
+<div class="pagetitle">
+  <h1>Barang Laundry</h1>
+  <nav>
+  </nav>
+</div>
+
+
+  <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Data Barang Laundry</h5>
+
+              <!-- Floating Labels Form -->
+              <form class="row g-3">
+                <div class="col-md-6">
+                  <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
+                    <label for="floatingName">Kode Pelanggan</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingEmail" placeholder="Your Email">
+                    <label for="floatingEmail">Kode Jenis</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Kode Barang Laundry</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingZip" placeholder="Zip">
+                    <label for="floatingCity">Jumlah</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingZip" placeholder="Zip">
+                    <label for="floatingCity">Status</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingZip" placeholder="Zip">
+                    <label for="floatingCity">Harga</label>
+                  </div>
+                </div>
+                <div class="text-end">
+                  <button type="submit" class="btn btn-info">Simpan</button>
+                  <button type="reset" class="btn btn-warning">Kembali</button>
+                </div>
+              </form><!-- End floating Labels Form -->
+
+            </div>
+          </div>
+</div>
+</main>
+<?= $this->endSection(); ?>
   <div class="card">
             <div class="card-body">
               <h5 class="card-title">Data Barang Laundry</h5>
