a1860cf648bb8c2001e5d43cb849b286b9f2ebee
rismauldamit <rismaulmagfiroh.plh123@gmail.com>
2024-06-04 15:47:05 +0800

    1234

--- app/Views/persediaan.php
+++ app/Views/persediaan.php
@@ -19,12 +19,26 @@
         </div>
     <div class="datatable-search">
             <input class="datatable-input" placeholder="Search..." type="search" name="search" title="Search within table">
-        </div>
+    </div>
 </div>
 <div class="datatable-container"><table class="table datatable datatable-table"><thead><tr><th data-sortable="true" style="width: 26.88296639629201%;"><button class="datatable-sorter">
                       <b>N</b>ame
-                    </button></th><th data-sortable="true" style="width: 9.154113557358054%;"><button class="datatable-sorter">Ext.</button></th><th data-sortable="true" style="width: 28.50521436848204%;"><button class="datatable-sorter">City</button></th><th data-format="YYYY/DD/MM" data-sortable="true" data-type="date" style="width: 16.917728852838934%;"><button class="datatable-sorter">Start Date</button></th><th data-sortable="true" class="red" style="width: 18.53997682502897%;"><button class="datatable-sorter">Completion</button></th></tr></thead><tbody><tr data-index="0"><td>Unity Pugh</td><td>9958</td><td>Curicó</td><td>2005/02/11</td><td class="green">37%</td></tr><tr data-index="1"><td>Theodore Duran</td><td>8971</td><td>Dhanbad</td><td>1999/04/07</td><td class="green">97%</td></tr><tr data-index="2"><td>Kylie Bishop</td><td>3147</td><td>Norman</td><td>2005/09/08</td><td class="green">63%</td></tr><tr data-index="3"><td>Willow Gilliam</td><td>3497</td><td>Amqui</td><td>2009/29/11</td><td class="green">30%</td></tr><tr data-index="4"><td>Blossom Dickerson</td><td>5018</td><td>Kempten</td><td>2006/11/09</td><td class="green">17%</td></tr><tr data-index="5"><td>Elliott Snyder</td><td>3925</td><td>Enines</td><td>2006/03/08</td><td class="green">57%</td></tr><tr data-index="6"><td>Castor Pugh</td><td>9488</td><td>Neath</td><td>2014/23/12</td><td class="green">93%</td></tr><tr data-index="7"><td>Pearl Carlson</td><td>6231</td><td>Cobourg</td><td>2014/31/08</td><td class="green">100%</td></tr><tr data-index="8"><td>Deirdre Bridges</td><td>1579</td><td>Eberswalde-Finow</td><td>2014/26/08</td><td class="green">44%</td></tr><tr data-index="9"><td>Daniel Baldwin</td><td>6095</td><td>Moircy</td><td>2000/11/01</td><td class="green">33%</td></tr></tbody></table></div>
-<div class="datatable-bottom">
+                    </button></th><th data-sortable="true" style="width: 9.154113557358054%;">
+                    <button class="datatable-sorter">Ext.</button></th><th data-sortable="true" style="width: 28.50521436848204%;">
+                    <button class="datatable-sorter">City</button></th><th data-format="YYYY/DD/MM" data-sortable="true" data-type="date" style="width: 16.917728852838934%;">
+                    <button class="datatable-sorter">Start Date</button></th><th data-sortable="true" class="red" style="width: 18.53997682502897%;">
+                    <button class="datatable-sorter">Completion</button></th></tr></thead>
+                    <tbody>
+                      <tr data-index="0">
+                        <td>Aya</td>
+                        <td>9958</td><td>Curicó</td>
+                        <td>2005/02/11</td>
+                        <td class="green">37%</td>
+                      </tr>
+                    </tbody>
+                    </table>
+</div>
+    <div class="datatable-bottom">
     <div class="datatable-info">Showing 1 to 10 of 100 entries</div>
     <nav class="datatable-pagination"><ul class="datatable-pagination-list"><li class="datatable-pagination-list-item datatable-hidden datatable-disabled"><button data-page="1" class="datatable-pagination-list-item-link" aria-label="Page 1">‹</button></li><li class="datatable-pagination-list-item datatable-active"><button data-page="1" class="datatable-pagination-list-item-link" aria-label="Page 1">1</button></li><li class="datatable-pagination-list-item"><button data-page="2" class="datatable-pagination-list-item-link" aria-label="Page 2">2</button></li><li class="datatable-pagination-list-item"><button data-page="3" class="datatable-pagination-list-item-link" aria-label="Page 3">3</button></li><li class="datatable-pagination-list-item"><button data-page="4" class="datatable-pagination-list-item-link" aria-label="Page 4">4</button></li><li class="datatable-pagination-list-item"><button data-page="5" class="datatable-pagination-list-item-link" aria-label="Page 5">5</button></li><li class="datatable-pagination-list-item"><button data-page="6" class="datatable-pagination-list-item-link" aria-label="Page 6">6</button></li><li class="datatable-pagination-list-item"><button data-page="7" class="datatable-pagination-list-item-link" aria-label="Page 7">7</button></li><li class="datatable-pagination-list-item datatable-ellipsis datatable-disabled"><button class="datatable-pagination-list-item-link">…</button></li><li class="datatable-pagination-list-item"><button data-page="10" class="datatable-pagination-list-item-link" aria-label="Page 10">10</button></li><li class="datatable-pagination-list-item"><button data-page="2" class="datatable-pagination-list-item-link" aria-label="Page 2">›</button></li></ul></nav>
 </div>
