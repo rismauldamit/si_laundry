87cbd5c93791ec439a7fa2d132eb0a3aec65e004
rismauldamit <rismaulmagfiroh.plh123@gmail.com>
2024-06-04 13:51:45 +0800

    qwerty

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -50,7 +50,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">Mama Laundry haha</span>
+        <span class="d-none d-lg-block">Mama Laundry</span>
       </a>
     </div><!-- End Logo -->
 
