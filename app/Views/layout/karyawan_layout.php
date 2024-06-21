c318262ceb2ba96e0a929ed5548056ddf37018cf
sherlyfs <sherlyfebriana56@gmail.com>
2024-06-21 23:03:10 +0800

    user lihat dan tambah

--- app/Views/layout/karyawan_layout.php
+++ app/Views/layout/karyawan_layout.php
@@ -10,23 +10,23 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= base_url(); ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
@@ -44,7 +44,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-      <img src="assets/img/laundry.png" alt="" style="width: 32px; height: auto;">
+        <img src="<?= base_url(); ?>assets/img/laundry.png" alt="" style="width: 32px; height: auto;">
         <span class="d-none d-lg-block">Mama Laundry</span>
       </a>
     </div><!-- End Logo -->
@@ -62,7 +62,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <i class="bi bi-columns-gap"></i>
+            <i class="bi bi-columns-gap"></i>
             <span class="d-none d-md-block dropdown-toggle ps-2"></span>
           </a><!-- End Profile Iamge Icon -->
 
@@ -118,14 +118,14 @@
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/pelanggan">
-        <i class='bx bx-group'></i>
+          <i class='bx bx-group'></i>
           <span>Pelanggan</span>
         </a>
       </li><!-- End F.A.Q Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/pembelian_barang">
-        <i class="bi bi-cart"></i>
+          <i class="bi bi-cart"></i>
           <span>Persediaan</span>
         </a>
       </li><!-- End Contact Page Nav -->
@@ -139,14 +139,14 @@
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/transaksi">
-        <i class='bx bxs-t-shirt'></i>
+          <i class='bx bxs-t-shirt'></i>
           <span>Transaksi</span>
         </a>
       </li><!-- End Login Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="/laporan">
-        <i class='bx bxs-report' ></i>
+          <i class='bx bxs-report'></i>
           <span>Laporan</span>
         </a>
       </li><!-- End Error 404 Page Nav -->
@@ -161,17 +161,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="assets/vendor/echarts/echarts.min.js"></script>
-  <script src="assets/vendor/quill/quill.js"></script>
-  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/echarts/echarts.min.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/quill/quill.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="<?= base_url(); ?>assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="<?= base_url(); ?>assets/js/main.js"></script>
 
 </body>
 
--- app/Views/user.php
+++ app/Views/user.php
@@ -4,8 +4,78 @@
 <main id="main" class="main">
   <div class="pagetitle">
     <h1>User</h1>
-    <nav>
-    </nav>
-  </div>
+
+    <?= $this->include('component/message'); ?>
+
+    <div class="card mt-2">
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-center">
+          <h5 class="card-title">Data User</h5>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahmodal">
+            + Tambah
+          </button>
+        </div>
+        <!-- Table with stripped rows -->
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Username</th>
+              <th scope="col">Password</th>
+              <th scope="col">Level/Role</th>
+              <th scope="col">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($listuser as $item) : ?>
+              <tr>
+                <th scope="row">1</th>
+                <td><?= $item['username']; ?></td>
+                <td><i>Password Disembunyikan</i></td>
+                <td><?= $item['role']; ?></td>
+                <td><button type="button" class="btn btn-warning btn-sm">Ubah</button>
+                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                </td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+        <!-- End Table with stripped rows -->
+
+        <!-- Modal Tambah Transaksi -->
+        <div class="modal fade" id="tambahmodal" tabindex="-1" aria-labelledby="tambahmodalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <form action="<?= base_url('/user'); ?>" class="modal-content" method="POST">
+              <div class="modal-header">
+                <h5 class="modal-title" id="tambahmodalLabel">Tambah User</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div class="mb-3">
+                  <label for="username" class="form-label">Username</label>
+                  <input name="username" type="text" class="form-control" id="username">
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input name="password" type="password" class="form-control" id="password">
+                </div>
+                <div class="mb-3">
+                  <label for="role" class="form-label">Level/Role</label>
+                  <select name="role" class="form-select" aria-label="Default select example">
+                    <option value="karyawan">Karyawan</option>
+                    <option value="admin">Admin</option>
+                  </select>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- Akhir Modal Tambah Transaksi -->
+      </div>
+    </div>
 </main>
 <?= $this->endSection(); ?>
